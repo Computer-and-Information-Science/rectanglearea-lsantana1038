93b296e91faa5f6898e78f01e6cfcd7fa92bf34d
lsantana1038 <lsantana1038@gmail.com>
2025-09-10 19:36:28 +0000

    Rectangle Properties

--- main.cpp
+++ main.cpp
@@ -5,5 +5,8 @@ int main() {
   const double length = 7.2;
   const double width = 1.3;
 
+  double area = length * width;
+  double perimeter = 2 * (length + width);
 
+  cout << "Rectangle Properties:" << endl;
 }
